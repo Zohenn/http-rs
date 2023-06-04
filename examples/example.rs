37a2c6342964a74cf1174d21399d946eed78a70f
Michal <mmarciniak299@gmail.com>
2023-06-04 13:37:07 +0200

    change project to be a library and run it from an example

---
+++ examples/example.rs
@@ -0,0 +1,6 @@
+use http_rs::run;
+use std::io::Result;
+
+fn main() -> Result<()> {
+    run()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,7 @@ fn handle_connection(stream: &mut TcpStream) -> Result<()> {
     Ok(())
 }
 
-fn main() -> Result<()> {
+pub fn run() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:80")?;
 
     for stream in listener.incoming() {
