ed8bd7acf184d11d538a9bee2d8959426440e146
Michal <mmarciniak299@gmail.com>
2023-06-04 12:55:46 +0200

    return 400 on malformed request

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use std::ops::Add;
 use crate::utils::StringUtils;
 
 fn handle_connection(stream: &mut TcpStream) -> Result<()> {
-    let mut raw_request = String::new();
+    // let mut raw_request = String::new();
     let mut stream_buf: [u8; 255] = [0; 255];
     let mut request_bytes: Vec<u8> = Vec::new();
 
@@ -22,7 +22,7 @@ fn handle_connection(stream: &mut TcpStream) -> Result<()> {
         match read_result {
             Ok(n) => {
                 request_bytes.extend_from_slice(stream_buf.take(n as u64).into_inner());
-                raw_request = raw_request.add(std::str::from_utf8(&stream_buf).unwrap());
+                // raw_request = raw_request.add(std::str::from_utf8(&stream_buf).unwrap());
                 if n < stream_buf.len() {
                     break;
                 }
@@ -35,14 +35,22 @@ fn handle_connection(stream: &mut TcpStream) -> Result<()> {
     // println!("Received message:\n{raw_request}\n");
 
     let request = parse_request(request_bytes.as_slice());
-    // todo: 400 if request is malformed
+
     println!("{request:#?}\n");
 
-    let mut response = Response::builder()
-        .status_code(ResponseStatusCode::Ok)
-        .header("Content-Type", "text/html; charset=utf-8")
-        .body(String::from("<html><body><h1>Hello</h2></body></html>").as_bytes_vec())
-        .get();
+    let mut response = if let Ok(request) = request {
+        Response::builder()
+            .status_code(ResponseStatusCode::Ok)
+            .header("Content-Type", "text/html; charset=utf-8")
+            .body(String::from("<html><body><h1>Hello</h1></body></html>").as_bytes_vec())
+            .get()
+    } else {
+        Response::builder()
+            .status_code(ResponseStatusCode::BadRequest)
+            .header("Content-Type", "text/html; charset=utf-8")
+            .body(String::from("<html><body><h1>400 Bad Request</h1></body></html>").as_bytes_vec())
+            .get()
+    };
 
     stream.write_all(&response.as_bytes())?;
 
